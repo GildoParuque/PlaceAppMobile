ce98d12602665d971c7c258219a8572c122f10fe
GildoParuque <gildoernestoparuque82@gmail.com>
2021-09-03 12:31:44 +0200

    Add project files.

---
+++ PlaceAppMobile/PlaceAppMobile/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("fa-brands-regular-400.otf", Alias = "FontAwesomeBrandRegular")]
+[assembly: ExportFont("fa-free-regular-400.otf", Alias = "FontAwesomeFreeRegular")]
+[assembly: ExportFont("fa-free-solid-900.otf", Alias = "FontAwesomeFreeSolid")]
---
+++ PlaceAppMobile/PlaceAppMobile/FontAwesome.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlaceAppMobile
+{
+   static class FontAwesomeSolid
+    {
+        //public const string ArrowLeft = "\uf060";
+        //public const string ArrowRight = "\uf061";
+        //public const string BookReader = "\uf5da";
+        //public const string CalendarAlt = "\uf073";
+        //public const string Cog = "\uf013";
+        //public const string HandHolding = "\uf060";
+        //public const string Music = "\uf001";
+        //public const string Search = "\uf002";
+        //public const string ShareAlt = "\uf1e0";
+        //public const string Suitcase = "\uf0f2";
+        //public const string Video = "\uf03d";
+
+        public const string CheckCircle = "\uf058";
+    }
+
+    static class FontAwesomeBrands
+    {
+        //public const string WhatsApp = "\uf232";
+    }
+    static class FontAwesomeRegular
+    {
+        //public const string Bell = "\uf0f3";
+        //public const string BellSlash = "\uf1f6";
+        //public const string Clock = "\uf017";
+        //public const string Futbol = "\uf1e3";
+        public const string CheckCircle = "\uf058";
+
+    }
+}
